8a0781076a08a563e155dc4d2057303c0da1bbeb
Dwayne Campogan <dcampogan@gmail.com>
2020-05-05 20:25:51 -0700

    Highlighting Errored Fields

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+//above "as" only done to avoid confusion (too many "reducers")
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
-
+  auth: authReducer,
+  form: formReducer
 });
