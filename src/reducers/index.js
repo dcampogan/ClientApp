aa70de4a5a15c5a76bfc194de145879e85af19b8
Dwayne Campogan <dcampogan@gmail.com>
2020-05-09 19:56:39 -0700

    Fetching a List of All Streams

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 //above "as" only done to avoid confusion (too many "reducers")
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 });
