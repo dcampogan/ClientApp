1ad342d243c3e8b9b74f8f4d17799da9ef91e3f0
Dwayne Campogan <dcampogan@gmail.com>
2020-05-10 19:55:20 -0700

    Manually Changing API Records

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from '../history';
 import { 
   SIGN_IN, 
   SIGN_OUT, 
@@ -23,13 +24,12 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, userId });
 
-    dispatch({
-      type: CREATE_STREAM,
-      payload: response.data,
-    });
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
   };
 };
 
